f3973912f12a5dab098806f3dc3cda0de63c9b46
Ocskó Gábor <ogabor@gmail.com>
2026-01-29 15:43:45 +0100

    Add HTTP router with health check endpoint

---
+++ internal/httpserver/router.go
@@ -0,0 +1,19 @@
+package httpserver
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func NewRouter() http.Handler {
+	r := chi.NewRouter()
+
+	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"ok":true}`))
+	})
+
+	return r
+}
